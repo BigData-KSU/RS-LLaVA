ce90fb5deef6cd890db6348287776170ad73750c
I Bagus Putu Swardanasuta <agusswardana123@gmail.com>
2024-12-12 07:08:32 +0700

    add attention dropout config

--- rsllava/model/builder.py
+++ rsllava/model/builder.py
@@ -49,6 +49,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
         if 'lora' in model_name.lower() and model_base is not None:
             from rsllava.model.language_model.llava_llama import LlavaConfig
             lora_cfg_pretrained = LlavaConfig.from_pretrained(model_path)
+            lora_cfg_pretrained.update({"attention_dropout": 0.1})
             tokenizer = AutoTokenizer.from_pretrained(model_base, use_fast=False)
             print('Loading LLaVA from base model...')
             model = LlavaLlamaForCausalLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=lora_cfg_pretrained, **kwargs)
